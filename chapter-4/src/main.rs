e194e5e28f820fcfac79d08d78b94c461f69cd7d
Dirga Yasa <cekingx@gmail.com>
2022-12-16 14:17:26 +0800

    chapter-4: take ownership

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -1,3 +1,17 @@
+fn takes_ownership(some_string: String) {
+    println!("string: {}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("number: {}", some_integer);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello");
+    takes_ownership(s);
+    let x = 5;
+    makes_copy(x);
+
+    println!("main string: {}", s);
+    println!("main number: {}", x);
 }
