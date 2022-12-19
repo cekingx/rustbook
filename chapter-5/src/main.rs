12ea65fa21f362a60e939de05a42117aa99a97ec
Dirga Yasa <cekingx@gmail.com>
2022-12-19 13:43:43 +0800

    chap-5: struct and update struct

--- chapter-5/src/main.rs
+++ chapter-5/src/main.rs
@@ -1,3 +1,27 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = build_user(String::from("someone@example.com"), String::from("someusername"));
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotheruser"),
+        ..user1
+    };
+
+    println!("user 1: {}", user1.email);
+    println!("user 2: {}", user2.email);
 }
