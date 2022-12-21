37ace3adaf29f2efe47770d9468bc0e86d2634cc
Dirga Yasa <cekingx@gmail.com>
2022-12-21 14:59:29 +0800

    chap-6: enum multiple type

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,13 +1,19 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
-} 
-
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
 fn main() {
-    let home = IpAddrKind::V4(127, 0, 0, 1);
-    let loopback = IpAddrKind::V6(String::from("::1"));
-    println!("home: {:?}", home);
-    println!("loopback: {:?}", loopback);
+    let quit = Message::Quit;
+    let moved = Message::Move { x: 1, y: 1 };
+    let write = Message::Write(String::from("message"));
+    let color = Message::ChangeColor(0, 255, 0);
+
+    println!("quit: {:?}", quit);
+    println!("move: {:?}", moved);
+    println!("write: {:?}", write);
+    println!("color: {:?}", color);
 }
