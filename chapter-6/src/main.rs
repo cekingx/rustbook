6fcfc59bbd312ab593819e6945d0991d0824fcb8
Dirga Yasa <cekingx@gmail.com>
2022-12-21 14:29:39 +0800

    chap-6: enum hold value

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,25 +1,13 @@
 #[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8, u8, u8, u8),
+    V6(String),
 } 
 
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    println!("Home: {:?}", home);
-    println!("Loopback: {:?}", loopback);
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+    println!("home: {:?}", home);
+    println!("loopback: {:?}", loopback);
 }
