9c0da6ed339136b7c4d7b9e1121756bf804920bd
Dirga Yasa <cekingx@gmail.com>
2022-12-21 14:23:57 +0800

    chap-6: enum inside struct

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+} 
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("Home: {:?}", home);
+    println!("Loopback: {:?}", loopback);
 }
