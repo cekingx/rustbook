4a6fa44b6be40ce5964ce58d78e332d90cf00170
Dirga Yasa <cekingx@gmail.com>
2022-12-22 21:51:13 +0800

    chap-6: match arm are expression

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,19 +1,23 @@
-#[derive(Debug)]
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
 fn main() {
-    let quit = Message::Quit;
-    let moved = Message::Move { x: 1, y: 1 };
-    let write = Message::Write(String::from("message"));
-    let color = Message::ChangeColor(0, 255, 0);
-
-    println!("quit: {:?}", quit);
-    println!("move: {:?}", moved);
-    println!("write: {:?}", write);
-    println!("color: {:?}", color);
+    let penny = Coin::Penny;
+    value_in_cents(penny);
 }
