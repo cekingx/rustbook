05fdb137af159e8eaaac34558dfa36e885e5fb9c
Dirga Yasa <cekingx@gmail.com>
2022-12-23 22:27:20 +0800

    chap-6: if let

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,14 +1,6 @@
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
-
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    println!("six: {:?}, none: {:?}", six, none);
+    let some_u8_value = Some(3u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    };
 }
