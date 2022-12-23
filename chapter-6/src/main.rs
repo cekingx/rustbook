60466cfc7637fb2f633309e1c30383e3ef392f89
Dirga Yasa <cekingx@gmail.com>
2022-12-23 21:50:45 +0800

    chap-6: match pattern

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,23 +1,30 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Florida,
+    Washington,
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny");
-            1
-        },
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        },
     }
 }
 
 fn main() {
-    let penny = Coin::Penny;
-    value_in_cents(penny);
+    let quarter = Coin::Quarter(UsState::Washington);
+    value_in_cents(quarter);
 }
