990242723a0bbb309f853152b9ac7e06ddc88f7b
Dirga Yasa <cekingx@gmail.com>
2022-12-26 17:02:53 +0800

    chap-7: test module

--- chapter-7/communicator/src/lib.rs
+++ chapter-7/communicator/src/lib.rs
@@ -5,3 +5,13 @@ pub mod network;
 pub fn message() {
   println!("some string from library");
 }
+
+#[cfg(test)]
+mod tests {
+  use super::client;
+
+  #[test]
+  fn it_works() {
+    client::connect();
+  }
+}
