55c536ee1dcf979101f82b13b98015a9747a64f4
Dirga Yasa <cekingx@gmail.com>
2022-12-26 16:54:45 +0800

    chap-7: bring multiple item to scope

--- chapter-7/src/main.rs
+++ chapter-7/src/main.rs
@@ -1,6 +1,18 @@
 use communicator;
 
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn one_module() {}
+            pub fn two_module() {}
+        }
+    }
+}
+
+use a::series::of::{one_module, two_module};
+
 fn main() {
     communicator::client::connect();
+    one_module();
     println!("Hello, world!");
 }
