a12a76931d450a369673d0a9e781b673ac2d8f39
Dirga Yasa <cekingx@gmail.com>
2022-12-27 13:59:45 +0800

    chap-8: vector enum

--- chapter-8/src/main.rs
+++ chapter-8/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+
+    let number = match row[0] {
+        SpreadsheetCell::Int(some) => some,
+        _ => 0
+    };
+
+    println!("number: {}", number);
+    println!("row: {:?}", row);
 }
