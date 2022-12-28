4c9c668082db4d810201a50e88669eb173ce94af
Dirga Yasa <cekingx@gmail.com>
2022-12-28 14:31:06 +0800

    chap-8: format macro

--- chapter-8/src/main.rs
+++ chapter-8/src/main.rs
@@ -1,22 +1,7 @@
 fn main() {
-    #[derive(Debug)]
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12)
-    ];
-
-    let number = match row[0] {
-        SpreadsheetCell::Int(some) => some,
-        _ => 0
-    };
-
-    println!("number: {}", number);
-    println!("row: {:?}", row);
+    let s1 = String::from("hello, ");
+    let s2 = String::from("world");
+    let s3 = format!("{}-{}", s1, s2);
+    println!("s3: {}", s3);
+    println!("s1: {}", s1);
 }
