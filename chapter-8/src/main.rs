d23e4a55134104bc533e608bb43a82f91f981d9a
Dirga Yasa <cekingx@gmail.com>
2023-01-09 17:54:46 +0800

    chap-8: hashmap insert

--- chapter-8/src/main.rs
+++ chapter-8/src/main.rs
@@ -1,9 +1,13 @@
 use std::collections::HashMap;
 
 fn main() {
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
 
-    println!("hashmap: {:?}", scores);
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
