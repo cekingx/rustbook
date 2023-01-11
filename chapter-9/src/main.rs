9608087d2889de721935fdd6d5cb53d253814bf5
Dirga Yasa <cekingx@gmail.com>
2023-01-11 18:04:15 +0800

    chap-9: propagating error

--- chapter-9/src/main.rs
+++ chapter-9/src/main.rs
@@ -1,21 +1,22 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::{io::{self, Read}, fs::File};
 
-fn main() {
+fn read_username_from_file() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
 
-    let f = match f {
+    let mut f = match f {
         Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => {
-                    panic!("Tried to create file but there was a problem: {:?}", e)
-                }
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        }
+        Err(e) => return Err(e),
     };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e)
+    }
+}
+
+fn main() {
+    let result = read_username_from_file();
+    println!("result: {:?}", result);
 }
