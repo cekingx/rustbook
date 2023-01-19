53358479bd1aeaccf7d2ead14f16ec8ab38c13da
Dirga Yasa <cekingx@gmail.com>
2023-01-19 21:52:40 +0800

    chap-10: mixed generic
    on struct method

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,28 +1,17 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
 struct Point<T, U> {
     x: T,
     y: U
 }
 
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point { x: self.x, y: other.y }
+    }
+}
+
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
-
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
-
-    let work = Point { x: 5, y: 4.0 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
