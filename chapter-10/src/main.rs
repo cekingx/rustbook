f6d0baa347f4ed250ea4135bc8832dded4b09bf6
Dirga Yasa <cekingx@gmail.com>
2023-01-19 20:45:50 +0800

    chap-10: generic on struct

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,4 +1,4 @@
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -10,24 +10,19 @@ fn largest_i32(list: &[i32]) -> i32 {
     largest
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
+    // let number_list = vec![34, 50, 25, 100, 65];
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
+
+    let work = Point { x: 5, y: 4.0 };
 }
