4dd45567c4eb778a37492299c2487650a9d52151
Dirga Yasa <cekingx@gmail.com>
2023-01-22 23:39:26 +0800

    chap-10: generic with trait bound

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,57 +1,23 @@
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+fn largest<T>(list: &[T]) -> T
+    where T: PartialOrd + Copy
+{
+    let mut largest = list[0];
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
     }
-}
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize_author(&self) -> String {
-        format!("{}", self.author)
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-}
-
-pub fn notify<T>(item: T) where T: Summary {
-    println!("Breaking news! {}", item.summarize());
+    largest
 }
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false
-    };
-    // println!("1 new tweet: {}", tweet.summarize());
-    notify(tweet);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from("The Pittsburgh Penguins once again are the best team in the NHL."),
-    };
-
-    println!("New article available! {}", article.summarize());
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
