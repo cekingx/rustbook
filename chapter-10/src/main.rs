64d2dedcd259665a33a055fd19ded10b6e6508ed
Dirga Yasa <cekingx@gmail.com>
2023-01-23 21:36:29 +0800

    chap-10: lifetime

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,30 +1,15 @@
-use std::fmt::Display;
-
-struct Pair<T> {
-    x: T,
-    y: T
-}
-
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self {
-            x,
-            y
-        }
-    }
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
-        }
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
 
 fn main() {
-    let pair = Pair::new(2,3);
-    pair.cmp_display();
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
