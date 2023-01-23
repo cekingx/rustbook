2655e6499f8d127164545ac84077ebc3709a432b
Dirga Yasa <cekingx@gmail.com>
2023-01-23 14:25:26 +0800

    chap-10: conditional method
    using trait bound for conditional method implementation

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,23 +1,30 @@
-fn largest<T>(list: &[T]) -> T
-    where T: PartialOrd + Copy
-{
-    let mut largest = list[0];
+use std::fmt::Display;
 
-    for &item in list {
-        if item > largest {
-            largest = item;
+struct Pair<T> {
+    x: T,
+    y: T
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y
         }
     }
+}
 
-    largest
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    let pair = Pair::new(2,3);
+    pair.cmp_display();
 }
