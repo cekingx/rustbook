b13f4da1fcdea4d758d8e57f86f6e103fa28bfe1
Dirga Yasa <cekingx@gmail.com>
2023-01-24 19:33:02 +0800

    chap-10: combine all
    generic, trait bound, lifetime

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,17 +1,21 @@
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
+use std::fmt::Display;
 
-impl<'a> ImportantExcerpt<'a> {
-    fn level(&self) -> i32 {
-        3
-    }
-
-    fn announce_and_return_part(&self, announcement: &str) -> &str {
-        println!("Attention please: {}", announcement);
-        self.part
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
 
 fn main() {
+    let string1 = String::from("abcd");
+    let string2 = String::from("xyz");
+    let result =
+        longest_with_an_announcement(string1.as_str(), string2.as_str(), String::from("listen"));
+    println!("result: {}", result);
 }
