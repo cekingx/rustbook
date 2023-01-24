dcf6abea32aa62f91cdd94a7a8424b15f8e7becf
Dirga Yasa <cekingx@gmail.com>
2023-01-24 19:05:31 +0800

    chap-10: lifetime elision

--- chapter-10/src/main.rs
+++ chapter-10/src/main.rs
@@ -1,14 +1,17 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
     }
 }
 
 fn main() {
-    let string1 = String::from("long string is long");
-    let string2 = String::from("xyz");
-    let result = longest(string1.as_str(), string2.as_str());
-    println!("The longest string is {}", result);
 }
