2c7c17e783c47fe9a65ef9f2dd16b4b69aa3f676
Dirga Yasa <cekingx@gmail.com>
2023-02-15 16:02:05 +0800

    chap-11: new test

--- chapter-11/silly-function/src/lib.rs
+++ chapter-11/silly-function/src/lib.rs
@@ -1,5 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
 }
 
 #[cfg(test)]
@@ -7,8 +8,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
