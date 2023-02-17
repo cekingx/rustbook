266e3fc70c1eb8b94df05b2ab8260eb47eca7457
Dirga Yasa <cekingx@gmail.com>
2023-02-17 14:45:36 +0800

    chap-11: test private function

--- chapter-11/add-two/src/lib.rs
+++ chapter-11/add-two/src/lib.rs
@@ -1,5 +1,9 @@
 pub fn add_two(a: i32) -> i32 {
-    a + 2
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -7,18 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn add_two_and_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn add_three_and_two() {
-        assert_eq!(5, add_two(3));
-    }
-
-    #[test]
-    #[ignore]
-    fn one_hundred() {
-        assert_eq!(102, add_two(100));
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2))
     }
 }
