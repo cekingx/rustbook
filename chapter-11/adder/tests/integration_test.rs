895d19b76c105047b3e3119a0433e0d27d1ae68d
Dirga Yasa <cekingx@gmail.com>
2023-02-17 15:21:36 +0800

    chap-11: integration test module

--- chapter-11/adder/tests/integration_test.rs
+++ chapter-11/adder/tests/integration_test.rs
@@ -1,7 +1,9 @@
 use adder;
+mod common;
 
 #[test]
 fn testing() {
+  common::setup();
   let rec1 = adder::Rectangle::create(8, 5);
   let rec2 = adder::Rectangle::create(9, 6);
   assert!(rec2.can_hold(&rec1));
