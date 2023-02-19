259cf17e2e7b2ab03445a2effbdbe26e3fd45a12
Dirga Yasa <cekingx@gmail.com>
2023-02-19 22:22:08 +0800

    chap-12: read file and args

--- chapter-12/src/main.rs
+++ chapter-12/src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong when reading the file");
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config {query, filename}
+    }
 }
