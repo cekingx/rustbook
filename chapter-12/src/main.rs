fd3c1c7b4bf1413710a412b6b3269f44399cccca
Dirga Yasa <cekingx@gmail.com>
2023-02-26 21:20:40 +0800

    chap-12: error handling

--- chapter-12/src/main.rs
+++ chapter-12/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -22,10 +26,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 }
