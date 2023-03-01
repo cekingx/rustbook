92ad8ff1ff7ec20558842f15b883406e332e8d9e
Dirga Yasa <cekingx@gmail.com>
2023-03-01 23:47:00 +0800

    chap-12: error to std error

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -15,8 +17,9 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config {query, filename})
+        Ok(Config {query, filename, case_sensitive})
     }
 }
 
@@ -49,7 +52,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename)?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
