9701ab6506933fe78e075a1adb3570bedae55792
Dirga Yasa <cekingx@gmail.com>
2023-03-03 15:21:00 +0800

    chap-13: iterator demo

--- chapter-13/src/main.rs
+++ chapter-13/src/main.rs
@@ -50,13 +50,13 @@ fn generate_workout(intensity: u32, random_number: u32) {
 }
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-
-    generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number
-    );
+    // let simulated_user_specified_value = 10;
+    // let simulated_random_number = 7;
+    //
+    // generate_workout(
+    //     simulated_user_specified_value,
+    //     simulated_random_number
+    // );
 }
 
 #[cfg(test)]
@@ -71,5 +71,35 @@ mod tests {
         let v2 = c.value(2);
 
         assert_eq!(v2, 2);
+    }
+}
+
+#[cfg(test)]
+mod another_test {
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1,2,3];
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1,2,3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_map() {
+        let v1: Vec<i32> = vec![1,2,3];
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(v2, vec![2,3,4]);
     }
 }
