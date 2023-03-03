46efca75ef42ae7909ad2b226fa9c228b683890f
Dirga Yasa <cekingx@gmail.com>
2023-03-03 15:58:06 +0800

    chap-12: update to iterator

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -10,16 +10,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config {query, filename, case_sensitive})
+        Ok(Config{query, filename, case_sensitive})
     }
 }
 
