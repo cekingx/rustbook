d97ae1ae886a522c05e912592d891d685e0fdcb1
Dirga Yasa <cekingx@gmail.com>
2023-03-09 23:50:41 +0800

    chap-15: deref trait

--- chapter-15/src/main.rs
+++ chapter-15/src/main.rs
@@ -1,19 +1,25 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
-use List::{Cons, Nil};
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
 
 fn main() {
-    let list = Cons(
-        1,
-        Box::new(Cons(
-            2,
-            Box::new(Cons(
-                3,
-                Box::new(Nil)
-            ))
-        ))
-    );
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
