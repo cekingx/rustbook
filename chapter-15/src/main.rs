78047b9b2b20e24350cc96bf91a91901b029cdb3
Dirga Yasa <cekingx@gmail.com>
2023-03-12 20:26:44 +0800

    chap-15: reference counted(Rc)

--- chapter-15/src/main.rs
+++ chapter-15/src/main.rs
@@ -1,20 +1,19 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data: `{}`!", self.data)
-    }
-}
+use std::rc::Rc;
+use List::{Cons, Nil};
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff")
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff")
-    };
-    drop(c);
-    println!("CustomSmartPointers created.");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
