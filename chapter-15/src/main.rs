d362f95c8f5f75697a6075f99aa35a337945e67f
Dirga Yasa <cekingx@gmail.com>
2023-03-14 01:17:30 +0800

    chap-15: reference cycle

--- chapter-15/src/main.rs
+++ chapter-15/src/main.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
@@ -8,16 +8,34 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use List::{Cons, Nil};
 
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
 fn main() {
-    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    *value.borrow_mut() += 10;
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+    // uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
