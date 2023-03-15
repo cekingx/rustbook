4478670ee93e31f0e9e949de42159a4d345f60e8
Dirga Yasa <cekingx@gmail.com>
2023-03-16 00:00:42 +0800

    chap-17: averaged collection

--- chapter-17/src/main.rs
+++ chapter-17/src/main.rs
@@ -1,3 +1,8 @@
+use averaged_collection::AveragedCollection;
+
 fn main() {
-    println!("Hello, world!");
+    let mut collection = AveragedCollection::new();
+    collection.add(1);
+    collection.add(2);
+    println!("the average: {}", collection.average());
 }
