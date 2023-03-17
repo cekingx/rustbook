78a88d9a84b58e7e03b2913aed509a226fcc530f
Dirga Yasa <cekingx@gmail.com>
2023-03-17 14:05:12 +0800

    chap-17: blog state pattern

--- chapter-17/src/main.rs
+++ chapter-17/src/main.rs
@@ -1,36 +1,14 @@
-use gui_like::{Draw, Button, Screen};
-
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        println!("draw select box");
-    }
-}
+use blog::Post;
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No")
-                ]
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("Ok"),
-            })
-        ]
-    };
+    let mut post = Post::new();
 
-    screen.run();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
