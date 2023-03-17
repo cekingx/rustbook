1aec8dc43947cde2afcc16b47e36d4e3e5df798c
Dirga Yasa <cekingx@gmail.com>
2023-03-17 14:36:59 +0800

    chap-17: blog alternative

--- chapter-17/src/main.rs
+++ chapter-17/src/main.rs
@@ -4,11 +4,9 @@ fn main() {
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
+    let post = post.approve();
 
-    post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
 }
