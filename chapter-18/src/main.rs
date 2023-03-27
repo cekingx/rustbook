4e57efc9cc3d027e6eb6aa91aee9a8c00c67834f
Dirga Yasa <cekingx@gmail.com>
2023-03-27 21:23:56 +0800

    chap-18: match range

--- chapter-18/src/main.rs
+++ chapter-18/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let x = 1;
+    let x = 5;
     match x {
-        1 | 2 => println!("one or two"),
-        3 => println!("three"),
-        _ => println!("anything")
+        1..=5 => println!("one through five"),
+        _ => println!("something else")
     }
 }
