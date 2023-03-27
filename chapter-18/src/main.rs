6b52607d86fe05bd2db0d8b92ab66be37556762e
Dirga Yasa <cekingx@gmail.com>
2023-03-27 21:27:02 +0800

    chap-18: match char range

--- chapter-18/src/main.rs
+++ chapter-18/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let x = 5;
+    let x = 'c';
     match x {
-        1..=5 => println!("one through five"),
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else")
     }
 }
