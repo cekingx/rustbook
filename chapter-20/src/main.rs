a6c978d53b16c4c4c88cfe0a7543d20ccbc7a331
Dirga Yasa <cekingx@gmail.com>
2023-04-17 14:47:06 +0800

    chap-20: multithread web server

--- chapter-20/src/main.rs
+++ chapter-20/src/main.rs
@@ -3,15 +3,19 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration
+    time::Duration,
 };
+use util::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
--- chapter-20/util/src/lib.rs
+++ chapter-20/util/src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of the threads in the pool
+    ///
+    /// # Panics
+    /// The `new` function will panic if the size is zero
+    ///
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock()
+                .unwrap()
+                .recv()
+                .unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker {id, thread}
     }
 }
